b38edc62807c085f59ac768efce6db15be9f3414
Grant Reighard <grant.reighard@gmail.com>
2018-11-16 13:33:02 -0500

    Tests passing on ex1 and ex2.

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -9,7 +9,17 @@ char **reconstruct_trip(Ticket **tickets, int length)
   HashTable *hash = create_hash_table(16);
   char **route = malloc(length * sizeof(char *));
 
-  // YOUR CODE HERE
+  for (int i = 0; i < length; i++) {
+    hash_table_insert(hash, tickets[i]->source, tickets[i]->destination);
+  }
+
+  char *firstPlace = hash_table_retrieve(hash, "NONE");
+  route[0] = firstPlace;
+  for (int i = 1; i < length; i++) {
+      route[i] = hash_table_retrieve(hash, route[i-1]);
+  }
+
+  destroy_hash_table(hash);
 
   return route;
 }
@@ -20,3 +30,32 @@ void print_route(char **route, int length)
     printf("%s\n", route[i]);
   }
 }
+
+#ifndef TESTING
+int main(void)
+{
+
+
+  Ticket **tickets = malloc(3 * sizeof(Ticket *));
+
+  Ticket *ticket_1 = malloc(sizeof(Ticket));
+  ticket_1->source = "NONE";
+  ticket_1->destination = "PDX";
+  tickets[0] = ticket_1;
+
+  Ticket *ticket_2 = malloc(sizeof(Ticket));
+  ticket_2->source = "PDX";
+  ticket_2->destination = "DCA";
+  tickets[1] = ticket_2;
+
+  Ticket *ticket_3 = malloc(sizeof(Ticket));
+  ticket_3->source = "DCA";
+  ticket_3->destination = "NONE";
+  tickets[2] = ticket_3;
+
+  // char *expected[] = { "PDX", "DCA", "NONE" };
+  print_route(reconstruct_trip(tickets, 3), 3);
+
+  return 0;
+}
+#endif
